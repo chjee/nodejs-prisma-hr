35b094e224229bbd41f60007ff4efe82e1886df0
Changhoon-Jee <chjee71@gmail.com>
2023-10-31 17:26:59 +0900

    Removed a disable option.

--- app.ts
+++ app.ts
@@ -9,35 +9,32 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const server = async () => {
-    
-    const app = express();
+  const app = express();
 
-    app.disable('x-powered-by');
+  app.use(morgan('dev'));
+  app.use(
+    cors({
+      origin: '*',
+      //   credentials: true,
+      methods: 'DELETE, POST, GET, PATCH, PUT, OPTIONS',
+    })
+  );
 
-    app.use(morgan('dev'))
-    app.use(
-        cors({
-            origin: '*',
-            //   credentials: true,
-            methods: 'DELETE, POST, GET, PATCH, PUT, OPTIONS',
-        })
-    );
+  app.use(express.json());
 
-    app.use(express.json());
+  app.use('/employee', routeEmployee);
+  app.use('/department', routeDepartment);
+  app.use('/subway', routeSubway);
 
-    app.use('/employee', routeEmployee);
-    app.use('/department', routeDepartment);
-    app.use('/subway', routeSubway);
+  app.use(commonErrProc);
 
-    app.use(commonErrProc);
-    
-    const listenPort = process.env.LISTEN_PORT || 8080;
-    const serverInstance = app.listen(listenPort, async () => {
-        serverInstance.keepAliveTimeout = 61000;
-        serverInstance.headersTimeout = 65000;
+  const listenPort = process.env.LISTEN_PORT || 8080;
+  const serverInstance = app.listen(listenPort, async () => {
+    serverInstance.keepAliveTimeout = 61000;
+    serverInstance.headersTimeout = 65000;
 
-        console.info('listening port', listenPort || 8080);
-    });
-}
+    console.info('listening port', listenPort || 8080);
+  });
+};
 
 server();
